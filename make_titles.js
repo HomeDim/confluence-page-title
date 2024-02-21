10b0627ffcdb4522b2ea205ef3b937ab5a03c0de
HomeDim <HomeDim@users.noreply.github.com>
2024-02-21 12:48:22 +0300

    Update make_titles.js
    Remove jQuery

--- make_titles.js
+++ make_titles.js
@@ -1,8 +1,11 @@
-$(document).on(
-    "mouseenter",
-    ".plugin_pagetree_children_span",
-    function() {
-        var linkText = $(this).find("a").text();
-        $(this).attr('title', linkText);
+const side_bar_content = document.querySelector('.acs-side-bar-content')
+
+side_bar_content.addEventListener(
+    'mouseover',
+    function(event){
+        if (event.target.closest('.plugin_pagetree_children_span')){
+            var linkText = event.target.innerText;
+            event.target.title = linkText;
+        }
     }
 );
