3b4f28722d6159449f24a447dbd2e1107a078517
Dmitry <HomeDim@users.noreply.github.com>
2024-02-27 20:54:27 +0300

    Update make_titles.js
    Added hostname checking

--- make_titles.js
+++ make_titles.js
@@ -1,11 +1,14 @@
-const side_bar_content = document.querySelector('.acs-side-bar-content')
+if (window.location.hostname.includes("confluence")) {
 
-side_bar_content.addEventListener(
-    'mouseover',
-    function(event){
-        if (event.target.closest('.plugin_pagetree_children_span')){
-            var linkText = event.target.innerText;
-            event.target.title = linkText;
+    const side_bar_content = document.querySelector('.acs-side-bar-content')
+
+    side_bar_content.addEventListener(
+        'mouseover',
+        function (event) {
+            if (event.target.closest('.plugin_pagetree_children_span')) {
+                var linkText = event.target.innerText;
+                event.target.title = linkText;
+            }
         }
-    }
-);
+    )
+};
